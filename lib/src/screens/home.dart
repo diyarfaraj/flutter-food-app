14b63adcae535a0445ec1192a70c316e504e0520
diyarfaraj <diyar.faraj@gmail.com>
2020-11-08 18:03:28 +0100

    added ground

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:foodApp/src/commons.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'what would you like to eat?',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.notifications_none), onPressed: () {}),
+                    Positioned(
+                      top: 10,
+                      right: 12,
+                      child: Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          color: red,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
